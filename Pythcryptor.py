48291df4617104f8896db7c7ff98d210edc6b625
Mateusz Adamczyk <matthew.a@tutanota.com>
2022-05-12 09:00:37 +0000

    Update Pythcryptor.py

--- Pythcryptor.py
+++ Pythcryptor.py
@@ -7,6 +7,7 @@ class pythCrypt(QWidget):
         programName = "Pythcryptor"
         infoLabel = "Welcome to Pythcryptor\n\n"
         defaultWindowSize = [int(400), int(600)]
+        currentCipher = "Null"
         def __init__(self, parent = None):
                 super().__init__(parent)
                 self.encryptTypeLabel = QLabel()
@@ -30,28 +31,26 @@ class pythCrypt(QWidget):
                 self.windowLayout.addRow(self.encryptTypeLabel)
                 self.windowLayout.addRow(self.encryptComboBox)
                 
-                #encryptComboBox.textActivated.connect(self.encryptCombotBox.trackChanges)
+                #self.encryptComboBox.textActivated.connect(self.buildChanges(self.encryptComboBox.currentText())) #Doesn't work for some reason lol. The cheat below.
+                self.encryptComboBox.textActivated.connect(lambda arg0 = self.encryptComboBox.currentText(), arg1 = self.currentCipher : self.buildChanges(arg0, arg1))
 
-                e2 = QLineEdit()
-                e2.setValidator(QDoubleValidator(0.99,99.99,2))
-                e3 = QLineEdit()
-                e3.setInputMask("+99_9999_999999")
-
-                e4 = QLineEdit()
-                e4.textChanged.connect(self.textchanged)
-
-                e5 = QLineEdit()
-                e5.setEchoMode(QLineEdit.Password)
-
-                e6 = QLineEdit("Hello PyQt5")
-                e6.setReadOnly(True)
-                e5.editingFinished.connect(self.enterPress)
+                #e2 = QLineEdit()
+                #e2.setValidator(QDoubleValidator(0.99,99.99,2))
+                #e3 = QLineEdit()
+                #e3.setInputMask("+99_9999_999999")
+                #e4 = QLineEdit()
+                #e4.textChanged.connect(self.textchanged)
+                #e5 = QLineEdit()
+                #e5.setEchoMode(QLineEdit.Password)
+                #e6 = QLineEdit("Hello PyQt5")
+                #e6.setReadOnly(True)
+                #e5.editingFinished.connect(self.enterPress)
 
                 self.setLayout(self.windowLayout)
 
-        def trackChanges(self):
-            if encryptComboBox.currentText() == "Ceasar":
-                global dialValue #ecs dee, the one liner global variable with initialization is not available. Prase Kek
+        def buildChanges(self, comboBoxSelection, currentCipher):
+            if comboBoxSelection == "Ceasar" and currentCipher != comboBoxSelection:
+                global dialValue #global shiftDial = QDial() #ecs dee, the one liner global variable with initialization is not available. Prase Kek
                 global shiftDial
                 shiftDial = QDial()
                 shiftDial.setSingleStep(1)
@@ -65,15 +64,14 @@ class pythCrypt(QWidget):
                 dialValue = QLabel("0")
                 dialValue.setFrameStyle(QFrame.Box | QFrame.Raised)
                 
-                layout.addRow(dialValue, shiftDial)
+                self.windowLayout.addRow(dialValue, shiftDial)
                 #if shiftDial.valueChanged():
                 #    dialValue = shiftDial.value()
+                self.trackChanges("Caesar")
+                
+        def trackChanges(self, currentCipher):
+            self.currentCipher = currentCipher
 
-        def textchanged(self,text):
-                print("Changed: " + text)
-
-        def enterPress(self):
-                print("Enter pressed")
 
 def window():
         app = QApplication(sys.argv)
